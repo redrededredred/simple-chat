ac973681563a731307dbb3d1d8fd6f31107d2a43
Chad van Basedman <113964826+redrededredred@users.noreply.github.com>
2024-06-10 10:09:14 +0200

    Update

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const { createServer } = require("node:http");
+const IO = require("socket.io")
 const app = express();
 const server = createServer(app);
+const io = IO(server);
+const { join } = require("node:path");
+const frontendPath = "C:\\Users\\Paul\\Documents\\GitHub\\simple-chat\\frontend\\"; // TODO: make less retarded
 const port = 3000;
 const host = "127.0.0.1";
 
+// Util functions
+function handleMessage(msg) {
+    io.emit("chatupdate", msg);
+};
+
 // Routes
 app.get("/", (req, res) => {
-    res.send("<h1>Test</h1>")
+    res.sendFile(join(frontendPath, "index.html"));
+});
+
+// Socket logic
+io.on("connection", (socket) => {
+    console.log("New socket connected!");
+    socket.on('disconnect', () => {
+        console.log('A socket disconnected!');
+      });
+    socket.on("chatmsg", (msg) => {
+        handleMessage(msg);
+    });
 });
 
 server.listen(port, host, () => {
